560d75991b6c3e6ec05531aa78c06a15f15c553c
Omid Meh <omid.meh@gmail.com>
2013-11-16 23:55:41 -0500

    fixed mult to mul

--- LabGit/LabGit/Omid.c
+++ LabGit/LabGit/Omid.c
@@ -7,7 +7,7 @@
 
 
 
-char* validInstruc[] = { "add", "sub", "addi", "mult", "lw", "sw", "beq", "haltSimulation" };
+char* validInstruc[] = { "add", "sub", "addi", "mul", "lw", "sw", "beq", "haltSimulation" };
 char* registers[] = { "$zero", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3", "$t0", "$t1",
 					  "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$s0", "$s1", "$s2", "$s3",
 					  "$s4", "$s5", "$s6", "$s7", "$t8", "$t9" };
